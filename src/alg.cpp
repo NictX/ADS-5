3b1ace27de5492e61bbda43c04ae86338bd2f34a
Nict <71667497+NictX@users.noreply.github.com>
2022-06-09 14:10:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,9 @@
-// Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
 #include "tstack.h"
 
-int priority(char i) {
-    switch (i) {
+int priority(char oper) {
+    switch (oper) {
         case '(': return 0;
         case ')': return 1;
         case '+': return 2;
@@ -16,71 +15,75 @@ int priority(char i) {
     }
 }
 
-int calculate(char i, int a, int b) {
-      switch (i) {
+int calculate(char oper, int a, int b) {
+      switch (oper) {
         case '+': return b + a;
         case '-': return b - a;
         case '*': return b * a;
         case '/':
-        if ( a != 0) return b / a;
+        if ( a != 0)
+        return b / a;
         default : return 0;
     }
 }
 
 std::string infx2pstfx(std::string inf) {
-  TStack<char, 20> stack1;
   std::string post;
+  TStack <char, 100> t;
   for (int i = 0; i < inf.size(); i++) {
     if (priority(inf[i]) == 5) {
-      post += inf[i];
-      post += ' ';
+        post += inf[i];
+        post += ' ';
     } else {
-      if (priority(inf[i]) == 0) {
-        stack1.push(inf[i]);
-      } else if (stack1.isEmpty()) {
-        stack1.push(inf[i]);
-      } else if (priority(inf[i]) > priority(stack1.get())) {
-        stack1.push(inf[i]);
-      } else if (priority(inf[i]) == 1) {
-        while (priority(stack1.get()) != 0) {
-          post += stack1.get();
-          post += ' ';
-          stack1.pop();
-        }
-        stack1.pop();
-      } else {
-        while (stack1.isEmpty() && (priority(inf[i]) <= priority(stack1.get()))) {
-            post += stack1.get();
+    if (priority(inf[i]) == 0) {
+        t.push(inf[i]);
+    } else if (t.isEmpty()) {
+       t.push(inf[i]);
+    } else if ((priority(inf[i]) > priority(t.get()))) {
+        t.push(inf[i]);
+    } else if (priority(inf[i]) == 1) {
+        while (priority(t.get()) != 0) {
+            post += t.get();
             post += ' ';
-            stack1.pop();
+            t.pop();
         }
-        stack1.push(inf[i]);
-      }
+        t.pop();
+    } else {
+        while (!t.isEmpty() && (priority(inf[i]) <= priority(t.get()))) {
+            post += t.get();
+            post += ' ';
+            t.pop();
+        }
+        t.push(inf[i]);
+    }
     }
   }
-  while (!stack1.isEmpty()) {
-    post += stack1.get();
+  while (!t.isEmpty()) {
+    post += t.get();
     post += ' ';
-    stack1.pop();
+    t.pop();
   }
-  post.pop_back();
+  for (int j = 0; j < post.size(); j++) {
+      if (post[post.size()-1] == ' ')
+      post.erase(post.size() - 1);
+  }
   return post;
 }
 
-int eval(std::string post) {
-  TStack<int, 50> stack2;
-  int math = 0;
-  for (int i = 0; i < post.size(); i++) {
-    if (priority(post[i]) == 5) {
-      stack2.push(post[i] - '0');
-    } else if (priority(post[i]) < 4) {
-      int x = stack2.get();
-      stack2.pop();
-      int y = stack2.get();
-      stack2.pop();
-      stack2.push(calculate(post[i], x, y));
+int eval(std::string pref) {
+TStack <int, 100> t;
+int res = 0;
+for (int i = 0; i < pref.size(); i ++) {
+    if (priority(pref[i]) == 5) {
+        t.push(pref[i] - '0');
+    } else if (priority(pref[i]) < 4) {
+        int x = t.get();
+        t.pop();
+        int y = t.get();
+        t.pop();
+        t.push(calculate(pref[i], x, y));
     }
-  }
-  math = stack2.get();
-  return math;
+}
+res = t.get();
+return res;
 }
